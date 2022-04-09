20b1f1176f58183fe3a781c8dfa99f87623dbdcc
Lawwest88 <lawrence-west@hotmail.com>
2022-04-09 08:57:42 -0500

    duck shooter start

---
+++ main.py
@@ -0,0 +1,44 @@
+from operator import truediv
+import pygame
+import os
+import time
+import random
+
+WIDTH, HEIGHT =750, 750 
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Duck Shooter")
+
+#loading images 
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
+
+#player ship
+DUCK_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_duck.png"))
+
+#lasers
+RED_LASER = pygame.image.load(os.path.join("assets","pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets","pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets","pixel_laser_blue.png"))
+DUCK_LASER = pygame.image.load(os.path.join("assets","pixel_laser_duck.png"))
+
+#background
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets","background-black.png")), (WIDTH, HEIGHT))
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+        pygame.display.update()
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+main()
