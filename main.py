bb4a823ef56188f3dcaca4f88d57e4233d7d69e8
Lawwest88 <lawrence-west@hotmail.com>
2022-04-09 11:45:14 -0500

    assigned control to player

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Duck Shooter")
 
 #loading images 
-RED_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
+RED_SPACE_player = pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
+GREEN_SPACE_player = pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
+BLUE_SPACE_player = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
 
-#player ship
-DUCK_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_duck.png"))
+#player player
+DUCK_SPACE_player = pygame.image.load(os.path.join("assets","pixel_ship_duck.png"))
 
 #lasers
 RED_LASER = pygame.image.load(os.path.join("assets","pixel_laser_red.png"))
@@ -26,19 +26,26 @@ DUCK_LASER = pygame.image.load(os.path.join("assets","pixel_laser_duck.png"))
 #background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets","background-black.png")), (WIDTH, HEIGHT))
 
-class Ship:
+class player:
     def __init__(self, x, y, health=100):
         self.x = x
         self.y = y
         self.health = health
-        self.ship_img = None
+        self.player_img = None
         self.laser_img = None
         self.laser = []
         self.cool_down_counter = 0
 
     def draw(self, window):
-        pygame.draw.rect(window, (255,0,0), (self.x, self.y, 50, 50))
+        window.blit(self.player_img, (self.x, self.y))
 
+class Player(player):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.player_img = DUCK_SPACE_player
+        self.laser_img = DUCK_LASER
+        self.mask = pygame.mask.from_surface(self.player_img)
+        self.max_health = health
 
 def main():
     run = True
@@ -49,7 +56,7 @@ def main():
 
     player_val = 5
 
-    ship = Ship(300, 650)
+    player = Player(300, 650)
 
     clock = pygame.time.Clock()
 
@@ -62,7 +69,7 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
-        ship.draw(WIN)
+        player.draw(WIN)
 
         pygame.display.update()
 
@@ -75,10 +82,10 @@ def main():
                 run = False
         
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and ship.x - player_val > 0: #left
-            ship.x -= player_val
-        if keys[pygame.K_d] and ship.x + player_val < WIDTH: #right
-            ship.x += player_val
+        if keys[pygame.K_a] and player.x - player_val > 0: #left
+            player.x -= player_val
+        if keys[pygame.K_d] and player.x + player_val + 50 < WIDTH: #right
+            player.x += player_val
         
         
 main()
